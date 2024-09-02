1b56b79d80f4fbf9a774fb10ee0ec8d08c71185f
Andee-WSL-22.04.6 <andeeolayinkascott@gmail.com>
2024-09-02 14:34:21 +0100

    feat: set-up app structure, configure db

---
+++ grand-chat-server/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CommonModule } from './common/common.module';
+import { UsersModule } from './users/users.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { MessagesModule } from './messages/messages.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().port().required(),
+      })
+    }),
+    // CommonModule,
+    UsersModule,
+    RoomsModule,
+    MessagesModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ grand-chat-server/src/common/common.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [],
+    synchronize: true,
+    }),
+    AuthModule
+  ]
+})
+export class CommonModule {}
